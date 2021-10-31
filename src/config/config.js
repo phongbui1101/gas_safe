895efcc0b8eaa9b70a00cdaa0237cbfbf94ff50d
phongbui1101 <82431994+phongbui1101@users.noreply.github.com>
2021-10-31 14:31:19 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
 //USER 
-export const USERNAME = 'phongbui1101'; //Add username
-export const KEY = 'aio_Bnef45UbBsMk1cid4dDbADKoij7l' // active key
+export const USERNAME = '........'; //Add username
+export const KEY = '........' // active key
 export const FEED_3_NAME = 'bk-iot-led'; // led add feed name.
 export const FEED_4_NAME = 'bk-iot-speaker'; // Buzzer add feed name.
 //USER 1
-export const USERNAME1 = 'phongbui1101'; //Add username1
-export const KEY1 = 'aio_Bnef45UbBsMk1cid4dDbADKoij7l' // active key1
+export const USERNAME1 = '........'; //Add username1
+export const KEY1 = '........' // active key1
 export const FEED_1_NAME = 'bk-iot-gas'; //sensor add feed name.
 export const FEED_2_NAME = 'bk-iot-relay'; // replay add feed name.
 
